5d127b8e6e0c93a4d906288d8510869621bc1e72
Hunter Phelps <Hphelps8@gmail.com>
2023-01-07 23:44:37 -0700

    Completed Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string PercentageString = Console.ReadLine();
+        int percentage = int.Parse(PercentageString);
+
+        string grade = "N/A";
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Better luck next time.");
+        }
+        Console.WriteLine($"You got a {grade}");
     }
 }
