a99fb639162afdf93b398270cd48fb5acee70dee
Hunter Phelps <Hphelps8@gmail.com>
2023-01-07 23:46:51 -0700

    Updated Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,5 +10,6 @@ class Program
         string last_name = Console.ReadLine();
         Console.WriteLine();
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine();
     }
 }
