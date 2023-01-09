ca71adece97aa140089d91237bda29887a82a3c2
Hunter Phelps <Hphelps8@gmail.com>
2023-01-09 09:59:59 -0700

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 101);
+        Console.WriteLine($"Guess the magic number 1-100");
+        int number = 0;
+        int guess = 0;
+        while (MagicNumber != number)
+        { 
+            Console.Write($"What is your guess? ");
+            number = int.Parse(Console.ReadLine());
+            if (MagicNumber > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            if (MagicNumber < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            guess = guess + 1;
+        }
+        Console.WriteLine($"You're Correct! You got it in {guess} guesses.");
     }
 }
