869b316f28863fdc71f4e7541b872c146ed8d5c8
Hunter Phelps <Hphelps8@gmail.com>
2023-01-21 21:11:02 -0700

    "Finished Abstraction prep"

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Intel";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Console.WriteLine(job1._jobTitle);
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job1._startYear);
+        Console.WriteLine(job1._endYear);
+        Console.WriteLine();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Cracker Jack Packager";
+        job2._company = "CJ INC.";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+
+        Console.WriteLine(job2._jobTitle);
+        Console.WriteLine(job2._company);
+        Console.WriteLine(job2._startYear);
+        Console.WriteLine(job2._endYear);
+        Console.WriteLine();
+
+        job1.Display();
+        job2.Display();
+        Console.WriteLine();
+
+        Resume myResume = new Resume();
+        myResume._name = "Hunter Phelps";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        Console.WriteLine();
     }
 }
